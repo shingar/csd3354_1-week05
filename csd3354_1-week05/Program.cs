8f8ae1d9e8f616278be439fba639d42a983324be
csd3354_1_week05 <shingarsingh1997@gmail.com>
2019-02-08 19:24:14 -0500

    commit6:10_

--- csd3354_1-week05/Program.cs
+++ csd3354_1-week05/Program.cs
@@ -6,10 +6,145 @@ using System.Threading.Tasks;
 
 namespace csd3354_1_week05
 {
+
     class Program
+
+
+
     {
+
+
+
         static void Main(string[] args)
+
+
+
         {
+
+
+
+            Elevator e = new Elevator();
+
+
+
+            e.run();
+
+
+
         }
+
+
+
     }
+
+
+
+
+
+
+
+    class Node
+
+
+
+    {
+
+
+
+        public Node() { }
+
+
+
+        public Node floor;
+
+
+
+        public Node elevatorUp;
+
+
+
+        public string FloorNumber;
+
+
+
+    }
+
+
+
+
+
+
+
+    class Elevator
+
+
+
+    {
+
+
+
+        Node FirstFloor;
+
+
+
+        Node SecondFloor;
+
+
+
+        Node ThirdFloor;
+
+
+
+        Node FourthFloor;
+
+
+
+
+
+
+
+        public void run()
+
+
+
+        {
+
+
+
+            FirstFloor = new Node();
+
+
+
+            FirstFloor.FloorNumber = "First Floor";
+
+
+
+            Console.WriteLine("floor number is {0} ", FirstFloor.FloorNumber);
+
+
+
+            FirstFloor.elevatorUp = SecondFloor;
+
+
+
+            SecondFloor.FloorNumber = "Second Floor";
+
+
+
+
+
+
+
+            Console.WriteLine("floor number is {0} ", SecondFloor.FloorNumber);
+
+
+
+        }
+
+
+
+    }
+
+
+
 }
